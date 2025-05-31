b5820a5321deddc27396144d5e5c3410715b5773
Shivanshu <ks7876555@gmail.com>
2025-05-31 12:43:25 +0530

    django basics

---
+++ django/myproject/myapp/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('info', views.info, name='info'),
+
+]
