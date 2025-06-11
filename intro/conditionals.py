d4bee67020a21633a4e8fe87bd53dec4f8e8dcba
Shivanshu <ks7876555@gmail.com>
2025-06-11 10:54:13 +0530

    conditionals

---
+++ intro/conditionals.py
@@ -0,0 +1,48 @@
+#  if
+#  nested if
+# if ladder
+# if else
+#  if elif else
+#  short hand if else
+
+
+# day = "jan"
+# weather = "sunny"
+
+# if day == "monday":
+#     print("Today is monday")
+#     print("happy monday")
+#     print("have a good day ")
+    
+# elif day == "tuesday":
+#     print("Today is tuesday")
+#     print("happy tuesday")
+#     print("have a good day ")
+    
+# elif day == "wednesday":
+#     print("Today is wednesday")
+#     print("happy wednesday")
+#     print("have a good day ")
+    
+# elif day == "thursday":
+#     print("Today is thursday")
+#     print("happy thursday")
+#     print("have a good day ")
+    
+# elif day == "friday":
+#     print("Today is friday")
+#     print("happy friday")
+#     print("have a good day ")
+# else:
+#     print("Have a nice day")
+    
+    
+    
+# if exp:
+#     # code
+# else:
+#     # code
+    
+# first if exp else second
+    
+print("true") if False else print("false")
